53e6e7a6253483d3e2f4a35578a3e585ebc3c600
Dario Cartagena <dariocartagena@gmail.com>
2024-05-16 22:33:08 -0400

    primer commit

---
+++ integracion/src/main/java/com/mycompany/integracion/Integracion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.integracion;
+
+/**
+ *
+ * @author JOSE DARIO CARTAGENA
+ */
+public class Integracion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
